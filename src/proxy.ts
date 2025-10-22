b44bd103b75a7a22304138c832d2284b96c6398c
Tiago Oliveira <tiagoco2008@gmail.com>
2025-10-22 23:59:03 +0100

    changed middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { verifyToken } from "@/lib/Jwt";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // 1. Read the cookie
   const token = request.cookies.get("auth_token")?.value;
   const valid = token && verifyToken(token);
